2f5c1e7808945ef663d740721139071947e0f81a
includePrince <prince.kr9835660@gmail.com>
2024-09-07 02:05:35 +0530

    Initial commit

---
+++ src/routes/main.js
@@ -0,0 +1,27 @@
+import React from 'react'
+const Home = React.lazy(
+    () => import('./../pages/Home/home'),
+)
+const Blog = React.lazy(
+    () => import('./../pages/Blog/blog'),
+)
+const Projects = React.lazy(
+    () => import('./../pages/Projects/projects'),
+)
+const Contact = React.lazy(
+    () => import('./../pages/Contact/contact'),
+)
+
+const Preloader = React.lazy(
+    () => import('./../components/preloader'),
+)
+
+var mainRoutes = [
+    { path: '/', name: 'Homepage', component: <Home /> },
+    { path: '/blog', name: 'Blog', component: <Blog /> },
+    { path: '/projects', name: 'Projects', component: <Projects /> },
+    { path: '/contact', name: 'Contact', component: <Contact /> },
+    { path: '/preloader', name: 'Preloader', component: <Preloader /> },
+]
+
+export default mainRoutes
