316996afac66d53786c80c840165f027917f88a4
includePrince <prince.kr9835660@gmail.com>
2024-09-07 20:06:39 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import './App.scss';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import indexRoutes from './routes/index'
 import Preloader from './components/preloader';
 
--- src/layout/main.js
+++ src/layout/main.js
@@ -1,32 +1,31 @@
-import React, { Suspense } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Header from '../pages/Header/header';
-import Footer from '../pages/Footer/footer';
-import Preloader from '../components/preloader';  // Import Preloader
-import './main.scss';
+import React, { Suspense } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Header from '../pages/Header/header'
+import Footer from '../pages/Footer/footer'
+import './main.scss'
 
-import mainRoutes from '../routes/main';
+import mainRoutes from '../routes/main'
 
 function MainLayout() {
     return (
         <div className="main">
             <Header />
-            <div className="content">
-                <Suspense fallback={<Preloader />}>  {/* Use Preloader as fallback */}
-                    <Routes>
-                        {mainRoutes.map((route, index) => (
+            <Suspense fallback={<p>Loading...</p>}>
+                <Routes>
+                    {mainRoutes.map((prop, key) => {
+                        return (
                             <Route
-                                key={route.path || index}
-                                path={route.path}
-                                element={route.component}
-                            />
-                        ))}
-                    </Routes>
-                </Suspense>
-            </div>
+                                path={prop.path}
+                                key={key}
+                                element={prop.component}
+                            ></Route>
+                        )
+                    })}
+                </Routes>
+            </Suspense>
             <Footer />
         </div>
     );
 }
 
-export default MainLayout;
+export default MainLayout
